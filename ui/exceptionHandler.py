eb2105415b925bef32e5b819333c9eb9e2dfced1
欸嘿欸嘿 <1436648676@qq.com>
2022-05-18 20:12:00 +0800

    fix(module): reduce imports

--- ui/exceptionHandler.py
+++ ui/exceptionHandler.py
@@ -1,10 +1,9 @@
 
-import tkinter
 import traceback
 import ttkbootstrap as ttk
 
 class exceptionHandler(ttk.Toplevel):
-    def __init__(self,master:tkinter.Misc,e:Exception) -> None:
+    def __init__(self,master,e:Exception) -> None:
         super().__init__()
         self.master = master
         self.exception = e
@@ -15,25 +14,17 @@ class exceptionHandler(ttk.Toplevel):
         self.resizable(False, False)
         # self.geometry("500x600")
 
-        self.label1 = ttk.Label(master=self,anchor=tkinter.W,font=font,text="程序运行过程中出现了错误，下面是错误信息。")
-        self.label2 = ttk.Label(master=self,anchor=tkinter.W,font=font,text="麻烦你把下列错误信息发送到 github.com/Fatpandac/fuck_cqooc/issues。我们很快就会修复的！")
-        self.label1.pack(fill=tkinter.BOTH,padx=10,pady=(10,0))
-        self.label2.pack(fill=tkinter.BOTH,padx=10,pady=(0,10))
+        self.label1 = ttk.Label(master=self,anchor=ttk.W,font=font,text="程序运行过程中出现了错误，下面是错误信息。")
+        self.label2 = ttk.Label(master=self,anchor=ttk.W,font=font,text="麻烦你把下列错误信息发送到 github.com/Fatpandac/fuck_cqooc/issues。我们很快就会修复的！")
+        self.label1.pack(fill=ttk.BOTH,padx=10,pady=(10,0))
+        self.label2.pack(fill=ttk.BOTH,padx=10,pady=(0,10))
 
-        self.stacktraceText = ttk.Text(master=self,wrap=tkinter.WORD)
-        self.stacktraceText.insert(tkinter.END,"请将本段信息发送到github.com/Fatpandac/fuck_cqooc/issues，使用快捷键Ctrl+A和Ctrl+C复制。\n")
-        self.stacktraceText.insert(tkinter.END,"===============================================\n")
-        self.stacktraceText.insert(tkinter.END,self.stacktrace)
-        self.stacktraceText.config(state=tkinter.DISABLED,font=font)
+        self.stacktraceText = ttk.Text(master=self,wrap=ttk.WORD)
+        self.stacktraceText.insert(ttk.END,"请将本段信息发送到github.com/Fatpandac/fuck_cqooc/issues，使用快捷键Ctrl+A和Ctrl+C复制。\n")
+        self.stacktraceText.insert(ttk.END,"===============================================\n")
+        self.stacktraceText.insert(ttk.END,self.stacktrace)
+        self.stacktraceText.config(state=ttk.DISABLED,font=font)
         self.stacktraceText.pack()
 
     def start(self) -> None:
         self.mainloop()
-
-if __name__ == "__main__":
-    try:
-        raise Exception("test")
-    except Exception as e:
-        root = tkinter.Tk()
-        handler = exceptionHandler(root,e)
-        handler.start()
