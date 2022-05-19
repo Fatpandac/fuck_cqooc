213b2e6ac70ec9f46f6a73a832430765c73a9922
欸嘿欸嘿 <1436648676@qq.com>
2022-05-20 00:20:47 +0800

    add(module): add packaging script for macos

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+APP = ['fuck_cqooc.pyw']
+OPTIONS = {
+    'argv_emulation': True,
+}
+
+setup(
+    app=APP,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app']
+)
